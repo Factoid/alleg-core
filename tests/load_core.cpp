e82008c223263f5a0cd2ae97fab698fd5d343162
Adrian Cheater <adrian@complexgames.com>
2014-03-12 00:26:50 -0500

    Exempted core file, added additional dump statments to load_core. Crashes on get ammo.

--- tests/load_core.cpp
+++ tests/load_core.cpp
@@ -21,6 +21,52 @@ int main( int argc, char** argv )
   {
     std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
   }
+  std::cout << "Drone Types\n";
+  for( auto p : *mission.GetDroneTypes() )
+  {
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
+  }
+  std::cout << "Developments\n";
+  for( auto p : *mission.GetDevelopments() )
+  {
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
+  }
+  std::cout << "Station Types\n";
+  for( auto p : *mission.GetStationTypes() )
+  {
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
+  }
+  std::cout << "Hull Types\n";
+  for( auto p : *mission.GetHullTypes() )
+  {
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
+  }
+  std::cout << "Expendable Types\n";
+  for( auto p : *mission.GetExpendableTypes() )
+  {
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << "\n";
+  }
+  std::cout << "Treasures\n";
+  for( auto p : *mission.GetTreasureSets() )
+  {
+    std::cout << "  " << p->GetName() << "\n";
+  }
+  std::cout << "Projectile Types\n";
+  for( auto p : *mission.GetProjectileTypes() )
+  {
+    std::cout << p->GetModelTexture() << " " << p->GetModelName() << " " << p->GetPower() << " " << p->GetSpeed() << "\n";
+  }
+  std::cout << "Ammo Pack\n";
+  {
+    auto p = mission.GetAmmoPack();
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
+  }
+  std::cout << "Fuel Pack\n";
+  {
+    auto p = mission.GetFuelPack();
+    std::cout << "  " << p->GetName() << " " << p->GetModelName() << " $" << p->GetPrice() << "\n";
+  }
+
   std::cout << "Shutting down\n";
   return 0;
 }
